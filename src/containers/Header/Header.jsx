5d8d886655edbf0b86b1dfca7a77e05bbd7d9ba7
Anton Kosar <kosarantoni@gmail.com>
2020-04-13 21:09:39 +0200

    Add menu

--- src/containers/Header/Header.jsx
+++ src/containers/Header/Header.jsx
@@ -1,16 +1,50 @@
-import React from "react";
+import React, {useState} from "react";
 import styles from "./Header.module.css"
 import logo from "../../Assets/logo.svg"
+import {CSSTransition, TransitionGroup} from "react-transition-group";
 
-let Header = () => {
-    return(
+const Header = () => {
+    let [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const menu =
+        <TransitionGroup>
+            {isMenuOpen && <CSSTransition
+                timeout={{
+                    enter: 0,
+                    exit: 300
+                }}
+                classNames={{
+                    enter: styles.menuEnter,
+                    enterActive: styles.menuEnter,
+                    enterDone: styles.menu,
+                    exit: styles.menu,
+                    exitActive: styles.menuEnter
+                }}>
+                <nav>
+                    <div className={styles.menuWrapper}>
+                        <div><a href="#welcomeSection" onClick={() => handleOpenMenu()}>Main</a></div>
+                        <div><a href="#classesSection" onClick={() => handleOpenMenu()}>Classes</a></div>
+                    </div>
+                </nav>
+            </CSSTransition>}
+        </TransitionGroup>;
+
+    const handleOpenMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
         <div className={styles.headerContainer}>
-            <button className={styles.menuButton}>
+            <button onClick={() => handleOpenMenu()}
+                    className={isMenuOpen ? styles.menuButton + " " + styles.menuOpen : styles.menuButton}>
                 <div className={styles.menuIcon}>
                     <div className={styles.bar}/>
                     <div className={styles.bar}/>
                     <div className={styles.bar}/>
-                </div>menu</button>
+                </div>
+                <span>menu</span>
+            </button>
+            {menu}
             <a className={styles.phoneNumber} href={"tel:+12 345 678 901"}>+12 345 678 901</a>
             <div className={styles.logo}>
                 <img src={logo} alt=""/>
